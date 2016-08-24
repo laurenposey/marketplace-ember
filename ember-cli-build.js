48c405bb5885a9be3098ead149def7446f5defe9
Lauren Posey and Olivia Hinton <lauren+olivia@>
2016-08-24 13:44:17 -0700

    added mroe styling tried to make it pretty

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,7 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/flexboxgrid/css/index.min.css');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
