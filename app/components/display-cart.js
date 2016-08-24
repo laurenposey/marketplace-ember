e333239bd79a0801a3809e2b9869e6f3c4472e45
Lauren Posey and Olivia Hinton <lauren+olivia@>
2016-08-24 11:42:13 -0700

    display and update the total

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+
+
+  totalCost: Ember.computed('shoppingCart.items.[]', function(){ //shopping Cart is the thing its observing
+    var total = 0;
+    this.get('shoppingCart.items').forEach(function(item) {
+      total += parseInt(item.get('price'));
+    });
+    return total;
+  })
+
 });
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  currentTotal: 0,
 
   add(item) {
     this.get('items').pushObject(item);
-  }
+    this.get('updateTotal')(this);
+  },
+
+  
 });
